87e17070a4a02776d5d586fdf71fedd5fcd9f45d
Xiongxin Zeng <zeng459@bu.edu>
2020-12-08 17:36:00 +0800

    Update PS6

--- PS1/PS1.P1.js
+++
@@ -1,5 +0,0 @@
-const reverseAlphaOrder = str => str.split("").sort().reverse().join("")
-// console.log(`The reverse alphabetic order for the string is:${reverseAlphaOrder('supercalifragilisticexpialidocious')}`)
-// console.log(reverseAlphaOrder("reverse1alphabetic2order"))
-
-module.exports = {reverseAlphaOrder};
